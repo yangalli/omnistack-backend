2cf9634a01b02754c13b0e814eb53365b6f1f3b4
yangalli <yangalli@hotmail.com>
2019-04-11 01:53:40 -0300

    Fixing cors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-app.use(cors);
+app.use(cors());
 
 const server = require('http').Server(app);
 const io = require("socket.io")(server);
